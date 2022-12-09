590d80f2d31200fe85c5ee7e194a4212d72eedee
Manimajo <120129806+Manimajo@users.noreply.github.com>
2022-12-09 11:39:05 +0100

    Update Mastodon_002.sh

--- Mastodon_002.sh
+++ Mastodon_002.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/rbenv.git /home/mastodon/.rbenv
 
 cd ~/.rbenv && src/configure && make -C src
 
@@ -8,7 +8,7 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/rbenv/ruby-build.git /home/mastodon/.rbenv/plugins/ruby-build
 
 RUBY_CONFIGURE_OPTS=--with-jemalloc rbenv install 3.0.4
 
