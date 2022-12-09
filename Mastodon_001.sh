7a2bf3a90e4fc640d8f686bb547f13167c635363
Manimajo <120129806+Manimajo@users.noreply.github.com>
2022-12-09 09:08:16 +0100

    Create Mastodon_001.sh

---
+++ Mastodon_001.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt install -y curl wget gnupg apt-transport-https lsb-release ca-certificates
+
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+
+wget -O /usr/share/keyrings/postgresql.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc
+
+echo "deb [signed-by=/usr/share/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/postgresql.list
+
+apt update
+
+apt install -y imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev nginx redis-server redis-tools postgresql postgresql-contrib certbot python3-certbot-nginx libidn11-dev libicu-dev libjemalloc-dev
+
+corepack enable
+
+yarn set version classic
+
+adduser --disabled-login mastodon
+
+su - mastodon
