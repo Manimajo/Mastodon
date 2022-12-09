6db86e72f6c0d122b6a5625463ec18cfe0d147e7
Manimajo <120129806+Manimajo@users.noreply.github.com>
2022-12-09 11:52:00 +0100

    Update Mastodon_002.sh

--- Mastodon_002.sh
+++ Mastodon_002.sh
@@ -4,9 +4,9 @@ git clone https://github.com/rbenv/rbenv.git /home/mastodon/.rbenv
 
 cd /home/mastodon/.rbenv && src/configure && make -C src
 
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/mastodon/.bashrc
 
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> /home/mastodon/.bashrc
 
 git clone https://github.com/rbenv/ruby-build.git /home/mastodon/.rbenv/plugins/ruby-build
 
