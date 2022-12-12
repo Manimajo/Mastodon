ae34d1230c3d45849a625d5b4f6aac871638e5e9
Manimajo <120129806+Manimajo@users.noreply.github.com>
2022-12-12 11:55:01 +0100

    Update Mastodon_005.sh

--- Mastodon_005.sh
+++ Mastodon_005.sh
@@ -4,6 +4,8 @@ cp /home/mastodon/live/dist/nginx.conf /etc/nginx/sites-available/mastodon
 
 ln -s /etc/nginx/sites-available/mastodon /etc/nginx/sites-enabled/mastodon
 
+sed -i 's/example.com/mastodon.impulsreha.local/g' /etc/nginx/sites-enabled/mastodon.conf
+
 cd
 
 cd ..
@@ -15,5 +17,3 @@ mkdir cert
 cd cert
 
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout example.key -out example.crt -subj "/CN=example.com" -addext "subjectAltName=DNS:example.com,DNS:www.example.net,IP:10.0.0.1"
-
-sed -i 's/example.com/mastodon.impulsreha.local/g' /etc/nginx/sites-enabled/mastodon.conf
