ab35abc692b8b1dd3d1c2d44dd9eb4bc5e11b685
Manimajo <120129806+Manimajo@users.noreply.github.com>
2022-12-12 15:08:35 +0100

    Update Mastodon_004.sh

--- Mastodon_004.sh
+++ Mastodon_004.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/mastodon/mastodon.git /home/mastodon/live
+sudo -u mastodon git clone https://github.com/mastodon/mastodon.git live && cd live
+
+sudo -u mastodon git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
+
+sudo -u mastodon bundle config deployment 'true'
+
+sudo -u mastodon bundle config without 'development test'
+
+sudo -u mastodon bundle install -j$(getconf _NPROCESSORS_ONLN)
+
+sudo -u mastodon yarn install --pure-lockfile
+
+sudo -u mastodon RAILS_ENV=production bundle exec rake mastodon:setup
