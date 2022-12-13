ce68e23fefe58b7ac3d1bfa329fab257718db2e9
Manimajo <120129806+Manimajo@users.noreply.github.com>
2022-12-13 13:32:06 +0100

    Update Mastodon_Git-Clone_001.sh

--- Mastodon_Git-Clone_001.sh
+++ Mastodon_Git-Clone_001.sh
@@ -4,6 +4,6 @@ git clone https://github.com/Manimajo/Mastodon /home/mastodon/Mastodon
 
 chmod 777 /home/mastodon/Mastodon/*
 
-cd /home/mastodon/Mastodon
+cp /home/mastodon/Mastodon/Mastodon_004.sh /home/mastodon/Mastodon_004.sh
 
 ./Mastodon_004.sh
