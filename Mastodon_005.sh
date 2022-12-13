4c9f549cebac80c1debf128206f183781c3e8b7e
Manimajo <120129806+Manimajo@users.noreply.github.com>
2022-12-13 08:59:13 +0100

    Update Mastodon_005.sh

--- Mastodon_005.sh
+++ Mastodon_005.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
 
-git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
-
-bundle config deployment 'true'
-
-bundle config without 'development test'
-
-bundle install -j$(getconf _NPROCESSORS_ONLN)
-
-yarn install --pure-lockfile
-
-sudo -u mastodon RAILS_ENV=production bundle exec rake mastodon:setup
