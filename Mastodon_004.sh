fd5913c997ed1e65f4874f04f3bd26ac87b21a54
Manimajo <120129806+Manimajo@users.noreply.github.com>
2022-12-14 11:09:24 +0100

    Update Mastodon_004.sh

--- Mastodon_004.sh
+++ Mastodon_004.sh
@@ -10,7 +10,7 @@ bundle config deployment 'true'
 
 bundle config without 'development test'
 
-bundle install -j$(getconf _NPROCESSORS_ONLN)
+sudo -u mastodon bundle install -j$(getconf _NPROCESSORS_ONLN)
 
 yarn install --pure-lockfile
 
