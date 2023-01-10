69b0a8500057a522cb7092c4dfc62b610c6fc7de
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-01-10 12:56:30 +0100

    Create Mastodon_007.sh

---
+++ Mastodon_007.sh
@@ -0,0 +1,27 @@
+chmod 777 -R /home
+
+chmod +x -R /
+
+systemctl start nginx
+
+systemctl restart nginx
+
+systemctl reload nginx
+
+cp /home/mastodon/live/dist/mastodon-web.service /etc/systemd/system/
+
+cp /home/mastodon/live/dist/mastodon-sidekiq.service /etc/systemd/system/
+
+cp /home/mastodon/live/dist/mastodon-streaming.service /etc/systemd/system/
+
+systemctl start mastodon-web
+
+systemctl start mastodon-sidekiq
+
+systemctl start mastodon-streaming
+
+systemctl enable mastodon-web
+
+systemctl enable mastodon-sidekiq
+
+systemctl enable mastodon-streaming
