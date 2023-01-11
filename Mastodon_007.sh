c5d0556234ad968afe997d7dae8c013d65ef44dc
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-01-11 13:17:06 +0100

    Update Mastodon_007.sh

--- Mastodon_007.sh
+++ Mastodon_007.sh
@@ -1,4 +1,4 @@
-sed -i 's/mastodon.local/mastodon.example.com/g' /etc/nginx/sites-available/mastodon.conf
+sed -i 's/mastodon.mastodon.local/mastodon.example.com/g' /etc/nginx/sites-available/mastodon.conf
 
 chmod 777 -R /home
 
