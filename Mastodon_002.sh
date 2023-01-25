7924617b1415c442198fed69b5540be2c7ff3298
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-01-25 14:08:38 +0100

    Update Mastodon_002.sh

--- Mastodon_002.sh
+++ Mastodon_002.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+git clone https://github.com/rbenv/rbenv.git /home/mastodon/.rbenv
+
+cd /home/mastodon/.rbenv && src/configure && make -C src
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/mastodon/.bashrc
+
+echo 'eval "$(rbenv init -)"' >> /home/mastodon/.bashrc
+
+git clone https://github.com/rbenv/ruby-build.git /home/mastodon/.rbenv/plugins/ruby-build
+
+chmod 777 -R /home/mastodon
+
+sudo -u mastodon rbenv install 3.0.4
+
+sudo -u mastodon rbenv global 3.0.4
+
+sudo -u mastodon apt install ruby full
+
 chmod 777 -R /var
 
-sudo -u mastodon apt install ruby
-
-chmod 777 -R /var
-
-chmod 777 /usr/local/bin
-
-sudo -u mastodon gem install bundler --no-document
