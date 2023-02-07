1c88ca88d68daeda2248b84eae5a6a236c0a3763
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-02-07 13:41:37 +0100

    Update Mastodon_004.sh

--- Mastodon_004.sh
+++ Mastodon_004.sh
@@ -20,7 +20,7 @@ sudo -u mastodon bundle install -j$(getconf _NPROCESSORS_ONLN)
 
 cd /home/mastodon/live
 
-sudo -u mastodon git checkout $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1) 
+sudo -u mastodon git checkout -f $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1) 
 
 sudo -u mastodon yarn install --pure-lockfile
 
