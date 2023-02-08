55b3c9ebd892e0517b311478cc8dcc456b276f1e
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-02-08 09:28:21 +0100

    Update Mastodon_003.sh

--- Mastodon_003.sh
+++ Mastodon_003.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-apt install postgresql postgresql-contrib
-
-apt install python3-software-properties
-
-apt install software-properties-common
-
-apt install ubuntu-advantage-tools
-
-apt install update-notifier-common
-
-apt-get update
-
-apt-get dist-upgrade
-
 sudo -u postgres bash <<EOF
 echo "In"
 
-sudo -u postgres psql
+psql
 
 CREATE USER mastodon CREATEDB;
 
