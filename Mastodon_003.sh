d9b6f116d1433c0dca627dfb812794f1d6ff638a
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-02-08 09:22:33 +0100

    Update Mastodon_003.sh

--- Mastodon_003.sh
+++ Mastodon_003.sh
@@ -2,6 +2,18 @@
 
 apt install postgresql postgresql-contrib
 
+apt install python3-software-properties
+
+apt install software-properties-common
+
+apt install ubuntu-advantage-tools
+
+apt install update-notifier-common
+
+apt-get update
+
+apt-get dist-upgrade
+
 sudo -u postgres bash <<EOF
 echo "In"
 
