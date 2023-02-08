7a8c489317416dcfc1c6754dd2f5458abcc1b601
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-02-08 12:17:19 +0100

    Update Mastodon_004.sh

--- Mastodon_004.sh
+++ Mastodon_004.sh
@@ -6,6 +6,8 @@ chmod 777 -R /home
 
 cd /home/mastodon/live
 
+git checkout -f $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
+
 chmod 777 -R /home/mastodon
 
 chmod 777 -R /home/mastodon
@@ -19,8 +21,6 @@ sudo -u mastodon bundle config without 'development test'
 sudo -u mastodon bundle install -j$(getconf _NPROCESSORS_ONLN)
 
 cd /home/mastodon/live
-
-git checkout -f $(git tag -l | grep -v 'rc[0-9]*$' | sort -V | tail -n 1)
 
 sudo -u mastodon yarn install --pure-lockfile
 
