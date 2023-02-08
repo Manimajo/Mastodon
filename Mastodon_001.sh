01fc142929ee7178b0008a4c24282b0657bf09c1
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-02-08 09:27:35 +0100

    Update Mastodon_001.sh

--- Mastodon_001.sh
+++ Mastodon_001.sh
@@ -20,6 +20,20 @@ apt update
 
 apt install -y imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev nginx redis-server redis-tools postgresql postgresql-contrib certbot python3-certbot-nginx libidn11-dev libicu-dev libjemalloc-dev
 
+apt install postgresql postgresql-contrib -y
+
+apt install python3-software-properties -y
+
+apt install software-properties-common -y
+
+apt install ubuntu-advantage-tools -y
+
+apt install update-notifier-common -y
+
+apt-get update -y
+
+apt-get dist-upgrade -y
+
 corepack enable
 
 yarn set version classic
