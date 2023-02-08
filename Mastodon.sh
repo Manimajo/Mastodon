c314ed61c8fb2c67ee86bff08ae3530abe3e2ecf
Manimajo <120129806+Manimajo@users.noreply.github.com>
2023-02-08 11:21:29 +0100

    Update Mastodon.sh

--- Mastodon.sh
+++ Mastodon.sh
@@ -3,17 +3,3 @@
 ./Mastodon_001.sh
 
 ./Mastodon_002.sh
-
-./Mastodon_003.sh
-
-./Mastodon_004.sh
-
-./Mastodon_005.sh
-
-./Mastodon_006.sh
-
-./Mastodon_007.sh
-
-./Mastodon_008.sh
-
-./Mastodon_009.sh
